010b57fbb282cd42f97bb1f700e70a23e9f7c219
Sufiyan  Yusuf <sufiyanyusuf78611@gmail.com>
2019-10-12 02:42:40 +0300

    added status api

--- src/Listing/listingRoute.js
+++ src/Listing/listingRoute.js
@@ -6,15 +6,15 @@ const Organization = require('../../db/models/organization');
 const ProductInfo = require('../../db/models/productInfo');
 
 ListingRouter.route('/updateInfo').put(async function (req, res) {
-  const newProdInfo = {title:req.body.title,
+  const newProdInfo = {
+    title:req.body.title,
     description:req.body.description,
     type:req.body.type,
     base_price:req.body.base_price,
     currency:req.body.currency,
     image_url:req.body.image_url
   };
-  try{
-         
+  try{    
     const updatedProductInfo = await ProductInfo.query()
     .patch(newProdInfo)
     .where('listing_id',req.body.listing_id)
@@ -28,6 +28,34 @@ ListingRouter.route('/updateInfo').put(async function (req, res) {
   }catch(e){
     console.error(e);
   }  
+})
+
+ListingRouter.route('/updateStatus').post(async function (req, res) {
+  console.log(req.body)
+  if (req.body.listing_id && req.body.status){
+    try{
+      const listing = await Listing.query()
+                                   .findById(req.body.listing_id)
+                                   .patch({
+                                     status:req.body.status
+                                   })
+      res.sendStatus(200)
+    }catch(e){
+      res.status(400)
+    }
+  }
+})
+
+ListingRouter.route('/:listing_id/status').get(async function (req, res) {
+  if (req.params.listing_id){
+    try{
+      const listing = await Listing.query().findById(req.params.listing_id)
+      const status = listing.status
+      res.json(status)
+    }catch(e){
+      res.status(400)
+    }
+  }
 })
 
 ListingRouter.route('/create').post(async function (req, res) {
